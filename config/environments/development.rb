334c60bf45e7edafeb16f7213073cd051214a2c0
Thomas Smith <thomas@apolloagriculture.com>
2025-10-24 14:48:51 +0200

    Make 'latest updates' optional in the home view

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :local
-  config.active_storage.service = :local_amazon
+  config.active_storage.service = :local
+  # config.active_storage.service = :local_amazon
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
